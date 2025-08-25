5a6f29c5d1733a5f652e89f1855b5959a755fedd
elyasafDana <elidnana@gmail.com>
2025-08-25 16:30:54 +0300

    Update Algorithms.hpp

--- q6/Algorithms.hpp
+++ q6/Algorithms.hpp
@@ -5,6 +5,6 @@
 namespace graph {
     class Algorithms {
     public:
-        static bool Euler_circle(const Graph& g, int start); // כבר קיים אצלך
+        static bool Euler_circle(const Graph& g, int start); 
     };
 }
