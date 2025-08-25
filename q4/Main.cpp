1c0159411028ab3045e253a067f94209ed78493e
elyasafDana <elidnana@gmail.com>
2025-08-25 16:29:29 +0300

    Update Main.cpp

--- q4/Main.cpp
+++ q4/Main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char* argv[]) {
     while ((opt = getopt(argc, argv, "v:e:r:")) != -1) {
         switch (opt) {
             case 'v':
-                vertex = atoi(optarg); // המרה ממחרוזת למספר
+                vertex = atoi(optarg); 
                 break;
             case 'e':
                 edge = atoi(optarg);
@@ -30,7 +30,7 @@ int main(int argc, char* argv[]) {
                 root = atoi(optarg);
                 break;
             case '?':
-                cerr << "שגיאה: אופציה לא מוכרת או חסר ערך" << endl;
+                cerr << "err" << endl;
                 return 1;
         }
     }
